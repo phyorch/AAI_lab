eb06db2926445015a4da2d1a9a50875232557350
Phyorch <405918147@qq.com>
2017-11-15 22:40:41 +0800

    commit

---
+++ lab8.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def train(images, one_hot_labels):
+    raise NotImplementedError
+
+
+def predict(images):
+    # Return a Numpy ndarray with the length of len(images).
+    # e.g. np.zeros((len(images),), dtype=int) means all predictions are 'airplane's
+    raise NotImplementedError
