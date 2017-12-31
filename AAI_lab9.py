4e3c837449d664bfaa27bab819f5153cecbdf821
phyorch <405918147@qq.com>
2017-12-31 15:11:07 +0800

    happy new year

--- AAI_lab9.py
+++ AAI_lab9.py
@@ -23,11 +23,16 @@ import time
 #import lib.stats as stats
 #from lib.hyperparams import *
 
-
-
-
+GAMA = 0.95
 ACTION_SPACE_SIZE = 4
-LEARNING_RATE = 0.2
+LEARNING_RATE = 0.02
+EPOCHS = 10
+EPOCH_SIZE = 100
+BACH_SIZE = 32
+EPSILON_UP = 0.1
+EPSILON_LOW = 0.01
+MEMORY_SIZE = 1000000
+EXPLORATION_SIZE = 3000000
 
 def model():
     'build our model'
@@ -45,3 +50,100 @@ def model():
     adam = Adam(lr= LEARNING_RATE)
     model.compile(loss='mse', optimizer= adam)
     return model
+
+
+'''observe is the first term 'observation' given by env.step()
+we resize the input to an 84*84 size image including to the paper
+at last we wan to get a 4 frame data which contain the imformation of motion'''
+def input_process(observe, state, init=False):
+    input = skimage.color.rgb2grey(observe)
+    input = skimage.transform.resize(input, (110,84), mode='constant', preserve_range=True)
+    input = skimage.util.crop(input, ((19,7),(0,0)))
+
+    if init==True:
+        state = np.stack((input, input, input, input), axis=-1)
+    else:
+        state.popleft()
+        state.append(input)
+    return state
+
+def epoch_initialize(env):
+    env.reset()
+    observation, reward, done, info = env.step()
+    state = input_process(observation, True)
+    epsilon = EPSILON_UP
+    return state, epsilon
+
+'''In this function, we use minibatch coming from dataset 
+to calculate the loss for the cov network to learn. 
+X is the input image(state)
+Y is the value list of Q as the effect of each action
+There is something important that in DQL, the loss function 
+is always changing because Q value will update at every episod'''
+def model_train(model, Dataset, loss):
+    minibatch = np.random.sample(Dataset, BACH_SIZE)
+    for i in range(BACH_SIZE):
+        state_prior, action, reward, state_post, done = minibatch[i]
+        if 'X' not in dir():
+            X = np.zeros((BACH_SIZE,state_prior[0],state_prior[1],state_prior[2]))
+        if 'Y' not in dir():
+            Y = np.zeros((BACH_SIZE,ACTION_SPACE_SIZE))
+        X[i] = state_prior
+        Y[i] = model.predict(state_prior)
+        Q_post = model.predict(state_post)
+
+        if done:
+            Y[i, action] = reward
+        else: Y[i, action] = reward + GAMA*np.max(Q_post)
+    loss += model.train_on_batch(X, Y)
+    return loss
+
+
+
+# we train the model EPOCHS times. In each epoch, we do traing by episod steps
+def model_run(model, env):
+    global epsilon
+    for epocch in range(EPOCHS):
+        reward_total = 0
+        episod = 0
+        Dataset = deque()
+        state_prior, epsilon = epoch_initialize(env)
+        for t in EPOCH_SIZE:
+            loss = 0
+            action = 0
+            reward = 0
+
+            if random.random()<=epsilon:
+                action = env.action_space.sample()
+            else:
+                Q_list = model.predict(state_prior)
+                action = np.argmax(Q_list)
+            if epsilon>EPSILON_LOW:
+                epsilon -= (EPSILON_UP-EPSILON_LOW)/EXPLORATION_SIZE
+
+            observation, reward, done, info = env.step(action)
+            if done:
+                episod +=1
+                print('game '+episod+' is over')
+                break
+            state_post = input_process(observation, state_prior)
+            reward_total += reward
+            Dataset.append((state_prior, action, reward, state_post, done))
+            if len(Dataset)>MEMORY_SIZE:
+                Dataset.popleft()
+            loss = model_train(model, Dataset, loss)
+            state_prior = state_post
+
+if __name__ == '__main__':
+    config = tf.ConfigProto
+    sess = tf.Session(config=config)
+    from keras import backend as K
+    K.set_session(sess)
+    K.set_image_dim_ordering('tf')
+
+
+
+
+
+
+
