c2c3bc2c5e39acabcb8e3fb05a75794a9f3f1556
phyorch <405918147@qq.com>
2018-01-04 15:02:59 +0800

    happy new year

--- AAI_lab9.py
+++ AAI_lab9.py
@@ -30,14 +30,16 @@ GAMA = 0.95
 ACTION_SPACE_SIZE = 4
 LEARNING_RATE = 0.02
 EPOCHS = 10
-EPOCH_SIZE = 300000
+EPOCH_SIZE = 30000
 REPORT = 200
 BACH_SIZE = 32
-OBSERVATION = 300
-EPSILON_UP = 0.1
+OBSERVATION = 1000
+EPSILON_UP = 0.2
 EPSILON_LOW = 0.01
 MEMORY_SIZE = 1000000
 EXPLORATION_SIZE = 3000000
+FILE_PATH = '/home/phyorch/Learning/AAI_lab/model.h5'
+WEIGHTS_PATH = '/home/phyorch/Learning/AAI_lab/model.h5'
 
 
 def setup():
@@ -89,9 +91,9 @@ def epoch_initialize(env):
     observation, reward, done, info = env.step(env.action_space.sample())
 
     state = input_process(observation, None, True)
-    epsilon = EPSILON_UP
+    #epsilon = EPSILON_UP
 
-    return state, epsilon
+    return state, reward
 
 '''In this function, we use minibatch coming from dataset 
 to calculate the loss for the cov network to learn. 
@@ -100,15 +102,15 @@ Y is the value list of Q as the effect of each action
 There is something important that in DQL, the loss function 
 is always changing because Q value will update at every episod'''
 def model_train(model, Dataset):
-    minibatch = np.random.sample(Dataset, BACH_SIZE)
+    minibatch = random.sample(Dataset, BACH_SIZE)
     for i in range(BACH_SIZE):
         state_prior, action, reward, state_post, done = minibatch[i]
         if 'X' not in dir():
-            X = np.zeros((BACH_SIZE,state_prior[0],state_prior[1],state_prior[2]))
+            X = np.zeros((tuple([BACH_SIZE])+ state_prior[0].shape))
         if 'Y' not in dir():
             Y = np.zeros((BACH_SIZE,ACTION_SPACE_SIZE))
         X[i] = state_prior
-        Y[i] = model.predict(state_prior)
+        #Y[i] = model.predict(state_prior)
         Q_post = model.predict(state_post)
 
         if done:
@@ -124,11 +126,13 @@ def model_train(model, Dataset):
 def model_run(model, env):
     #report = 0
     global epsilon
+    epsilon = EPSILON_UP
     for epocch in range(EPOCHS):
-        reward_total = 0
+        print('---------new epochs---------')
+        #game_reward = 0
         episod = 0
         Dataset = deque()
-        state_prior, epsilon = epoch_initialize(env)
+        state_prior, game_reward = epoch_initialize(env)
         for t in range(EPOCH_SIZE):
             env.render()
             loss = 0
@@ -144,12 +148,18 @@ def model_run(model, env):
                 epsilon -= (EPSILON_UP-EPSILON_LOW)/EXPLORATION_SIZE
 
             observation, reward, done, info = env.step(action)
+            if reward!=0:
+                print(reward)
             if done:
-                episod +=1
+                print('runing times:  ', time.time())
+                print('total reward:  ', game_reward)
+                print('current epsilon:  ', epsilon)
                 print('game ', episod, ' is over')
-                break
+                state_prior, game_reward = epoch_initialize(env)
+                model.save_weights(FILE_PATH)
+                continue
             state_post = input_process(observation, state_prior)
-            reward_total += reward
+            game_reward += reward
             Dataset.append((state_prior, action, reward, state_post, done))
             if len(Dataset)>MEMORY_SIZE:
                 Dataset.popleft()
@@ -157,11 +167,13 @@ def model_run(model, env):
                 print('--Observation is over, training now')
             if t>OBSERVATION:
                 model_train(model, Dataset)
-            if t/REPORT==0:
+            '''if t/REPORT==0:
                 print('runing times:  ', time.time())
-                print('total reward:  ',reward_total)
-                print('current epsilon:  ', epsilon)
+                print('total reward:  ',game_reward)
+                print('current epsilon:  ', epsilon)'''
             state_prior = state_post
+            if epocch==EPOCHS-1:
+                model.save_weights(FILE_PATH)
 
 
 
@@ -175,4 +187,6 @@ if __name__ == '__main__':
     #K.set_session(sess)
     #K.set_image_dim_ordering('tf')
     model = model()
+    model.load_weights(WEIGHTS_PATH)
     model_run(model, env)
+
